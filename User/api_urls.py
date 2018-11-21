67c9e6af3ffa4a02991ad85de0223ab6f4bcc2ac
wly <1241076850@qq.com>
2018-11-21 11:52:07 +0800

    add logout api

--- User/api_urls.py
+++ User/api_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from User.views import CreateUserView, LoginView
+from User.views import CreateUserView, LoginView, Logout
 
 urlpatterns = {
     path('create', CreateUserView.as_view()),
     path('login', LoginView.as_view()),
+    path('logout', Logout.as_view()),
 }
